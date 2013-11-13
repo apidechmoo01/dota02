68e34eed9fc3495cadc0165a58cab74fa8b96d92
apidechmoo01 <apidech_mo@hotmail.com>
2013-11-13 20:14:24 +0700

    update

---
+++ BuilderTree.java
@@ -0,0 +1,17 @@
+
+import java.util.Random;
+class BuilderTree extends CreateNode{
+ public static final int MAX_LEVEL = 8;
+ 
+ private Random random;
+ 
+ public BuilderTree() {
+  random = new Random();
+ }
+ 
+ //this method will build the tree that has the max level is eight.
+ public CreateNode build() {
+ return null;
+ }
+}
+
