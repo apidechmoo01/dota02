47fd1ce369523ce4f5fa2099e50e51a132b6d4a5
apidechmoo01 <apidech_mo@hotmail.com>
2013-11-13 20:21:08 +0700

    update

---
+++ FrameArt.java
@@ -0,0 +1,25 @@
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+class FrameArt extends JFrame {
+  
+  //To create the random Button
+  public void randomButton(){
+  }
+  
+  //Tthis method is a function of random button to create the new picture
+  public void toRandom(){
+  }
+  
+  //This method will create the text field to show about function of random art
+  public JTextField makeTextField(){
+      return null;
+  }
+  
+  //To create the cancel Button
+  public void cancelButton(){
+  }
+}
